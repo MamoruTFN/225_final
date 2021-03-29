4cd83b2bdfd99008adac9a6fa1bba3fbd9138da6
MamoruTFN <paweenwich.t@ku.th>
2021-03-29 18:20:55 +0700

    loob like

--- 6320502461_final_2.c
+++ 6320502461_final_2.c
@@ -4,12 +4,12 @@ int main()
 {
     int n,l[10000],a,max,c,c1;
     scanf("%d",&n);
-    for(int i=0;i<10000;i++)
+    for(int i=0; i<10000; i++)
     {
         l[i]=0;
     }
 
-    for(int i=0;i<n;i++)
+    for(int i=0; i<n; i++)
     {
         scanf("%d",&a);
         l[a-1]++;
@@ -17,20 +17,21 @@ int main()
 
     max=l[0];
 
-    for(int i=0;i<10000;i++)
+    for(int i=0; i<10000; i++)
     {
         if(l[i]>=max)
         {
             max=l[i];
-            c=i;
-            if(l[i]==max)
-            {
-                c1=i;
-            }
         }
     }
 
-        printf("%d",c+1);
+    for(int i=0;i<10000;i++)
+    {
+        if(l[i]==max)
+        {
+            printf("%d ",i+1);
+        }
+    }
 
 
 
